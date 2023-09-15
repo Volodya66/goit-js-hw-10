85690bd64d8c49d44019f3ce68c2a3603488481d
Volodya66 <130245015+Volodya66@users.noreply.github.com>
2023-09-15 12:14:54 +0300

    Update cat-api.js

--- js/cat-api.js
+++ js/cat-api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = `https://api.thecatapi.com/v1/`;
 const END_POINT = "breeds";
-const API_KEY = "live_tPwwbnWP694gnm07CqjMHUl9HU1UtcKQoJwQw8smvtjUU8kPon5Y9KAhe51haAcJ";
+const API_KEY = "live_YrZoFL1bRRDvmknNAS1vqCdheO56o5dsRh3gnt21QBtoytfrLFrWuXMm70JjIoaU";
 
 axios.defaults.headers.common["x-api-key"] = API_KEY;
 axios.defaults.baseURL = BASE_URL;
